7ce25666ced1ea33b899b2729e5ceabb3778c244
luis velado <luis_v@live.com>
2023-09-08 14:35:39 -0600

    boton con nombre para prueva

--- Rompecabezas/Form1.cs
+++ Rompecabezas/Form1.cs
@@ -9,7 +9,7 @@ namespace Rompecabezas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-                //no hay q copiar y pegar, solo tomar ideas y aplicar nuestra logica
+            //no hay q copiar y pegar, solo tomar ideas y aplicar nuestra logica
         }
     }
 }

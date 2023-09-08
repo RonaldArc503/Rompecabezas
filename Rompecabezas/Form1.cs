e7ae6bbd5b3adde7410fcb4d1e9b91a9ac6076ed
RonaldArc503 <122319950+RonaldArc503@users.noreply.github.com>
2023-09-08 14:14:07 -0600

    Rompecabeza p

--- Rompecabezas/Form1.cs
+++ Rompecabezas/Form1.cs
@@ -6,5 +6,10 @@ namespace Rompecabezas
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                //no hay q copiar y pegar, solo tomar ideas y aplicar nuestra logica
+        }
     }
 }

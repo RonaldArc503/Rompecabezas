855d6b4af24758244efd49262b6351500624812c
RodriGC12 <rodrigo.giron@catolica.edu.sv>
2023-09-09 20:29:13 -0600

    prueba

--- Rompecabezas/Form1.Designer.cs
+++ Rompecabezas/Form1.Designer.cs
@@ -28,50 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(66, 161);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 44);
-            button1.TabIndex = 0;
-            button1.Text = "luis velado ";
-            button1.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(66, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "luis velado ";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(66, 84);
-            button2.Name = "button2";
-            button2.Size = new Size(101, 38);
-            button2.TabIndex = 1;
-            button2.Text = "Ronald";
-            button2.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(66, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ronald";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(66, 251);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 44);
-            button3.TabIndex = 2;
-            button3.Text = "Erick";
-            button3.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(66, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Erick";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(500, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Rodrigo Giron";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(925, 449);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 449);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
@@ -79,5 +90,6 @@
         private Button button1;
         private Button button2;
         private Button button3;
+        private Button button4;
     }
 }

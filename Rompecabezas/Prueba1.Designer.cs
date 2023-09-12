36faf9f797226dd5cee2cbe0defcb35744991d47
RonaldArc503 <122319950+RonaldArc503@users.noreply.github.com>
2023-09-11 18:45:57 -0600

    Practica1

---
+++ Rompecabezas/Prueba1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Rompecabezas
+{
+    partial class Prueba1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picture0 = new PictureBox();
+            picture1 = new PictureBox();
+            picture2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)picture0).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picture1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picture2).BeginInit();
+            SuspendLayout();
+            // 
+            // picture0
+            // 
+            picture0.BorderStyle = BorderStyle.Fixed3D;
+            picture0.Location = new Point(428, 46);
+            picture0.Name = "picture0";
+            picture0.Size = new Size(340, 233);
+            picture0.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture0.TabIndex = 0;
+            picture0.TabStop = false;
+            picture0.DragDrop += picture0_DragDrop;
+            picture0.DragEnter += picture0_DragEnter;
+            picture0.MouseDown += picture0_MouseDown;
+            // 
+            // picture1
+            // 
+            picture1.BorderStyle = BorderStyle.Fixed3D;
+            picture1.Image = Properties.Resources._01_Visit_Juayua_Affordability_shutterstock_468064517;
+            picture1.Location = new Point(12, 12);
+            picture1.Name = "picture1";
+            picture1.Size = new Size(129, 106);
+            picture1.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture1.TabIndex = 1;
+            picture1.TabStop = false;
+            picture1.Click += picture1_Click;
+            picture1.DragDrop += picture1_DragDrop;
+            picture1.DragEnter += picture1_DragEnter;
+            picture1.MouseDown += picture1_MouseDown;
+            // 
+            // picture2
+            // 
+            picture2.BorderStyle = BorderStyle.Fixed3D;
+            picture2.Location = new Point(165, 12);
+            picture2.Name = "picture2";
+            picture2.Size = new Size(129, 106);
+            picture2.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture2.TabIndex = 2;
+            picture2.TabStop = false;
+            picture2.Click += picture2_Click;
+            picture2.DragDrop += picture2_DragDrop;
+            picture2.DragEnter += picture2_DragEnter;
+            picture2.MouseDown += picture2_MouseDown;
+            // 
+            // Prueba1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(picture2);
+            Controls.Add(picture1);
+            Controls.Add(picture0);
+            Name = "Prueba1";
+            Text = "Prueba1";
+            Load += Prueba1_Load;
+            ((System.ComponentModel.ISupportInitialize)picture0).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picture1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picture2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private PictureBox picture0;
+        private PictureBox picture1;
+        private PictureBox picture2;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+    }
+}
---
+++ Rompecabezas/Prueba1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rompecabezas
+{
+    public partial class Prueba1 : Form
+    {
+        public Prueba1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Prueba1_Load(object sender, EventArgs e)
+        {
+            picture0.AllowDrop = true;
+            picture1.AllowDrop = true;
+            picture2.AllowDrop = true;
+            // picture3.AllowDrop = false;
+            //  picture4.AllowDrop = false;
+
+           
+        }
+
+
+        // Efecto a entrar a Picture x
+        private void picture0_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void picture0_DragDrop(object sender, DragEventArgs e)
+        {
+            picture0.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+
+
+        }
+
+
+
+
+
+        private void picture2_DragDrop(object sender, DragEventArgs e)
+        {
+            picture2.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            e.Effect = DragDropEffects.Move;// <>
+        }
+
+        private void picture2_DragEnter(object sender, DragEventArgs e) // <>
+        {
+            e.Effect = e.AllowedEffect;
+        }
+
+        private void picture1_DragDrop(object sender, DragEventArgs e)
+        {
+            picture1.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);  // <>
+
+        }
+
+        private void picture1_DragEnter(object sender, DragEventArgs e) // <>
+        {
+            e.Effect = e.AllowedEffect;
+
+        }
+
+
+        // regresar foto e eliminar 
+        private void picture0_MouseDown(object sender, MouseEventArgs e)
+        {
+            picture0.DoDragDrop(picture0.Image, DragDropEffects.Move);
+            picture0.Image = null;
+        }
+        private void picture1_MouseDown(object sender, MouseEventArgs e)
+        {
+            picture1.DoDragDrop(picture1.Image, DragDropEffects.Move);
+            picture1.Image = null; // quita imagen del picture 1 al pasarlo a picture 0 "la grande foto"
+
+        }
+
+        private void picture2_MouseDown(object sender, MouseEventArgs e)
+        {
+            picture2.DoDragDrop(picture2.Image, DragDropEffects.Move);
+            picture2.Image = null;
+        }
+
+        private void picture3_MouseDown(object sender, MouseEventArgs e)
+        {
+            // picture3.DoDragDrop(picture3.Image, DragDropEffects.Move);
+            //   picture3.Image = null;
+        }
+
+        private void picture4_MouseDown(object sender, MouseEventArgs e)
+        {
+            // picture4.DoDragDrop(picture4.Image, DragDropEffects.Move);
+            //   picture4.Image = null;
+        }
+
+        private void picture2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picture1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        //termina eliminar y pasar
+
+
+    }
+}
+
+
